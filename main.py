7d49d87493db66b559684e7ab17a85efd65abc09
IvanSedykh <sed.ivan.dm@gmail.com>
2019-06-23 15:28:40 +0300

    beta - version added an ability to select style

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from model import StyleTransferModel
+from fast_neural_style.neural_style.neural_style import transfer
 from telegram_token import token
 import numpy as np
 from PIL import Image
@@ -10,7 +11,7 @@ from io import BytesIO
 
 model = StyleTransferModel()
 first_image_file = { }
-
+info = {}
 
 def send_prediction_on_photo(bot, update):
 	# Нам нужно получить две картинки, чтобы произвести перенос стиля, но каждая картинка приходит в
@@ -33,7 +34,8 @@ def send_prediction_on_photo(bot, update):
 		style_image_stream = BytesIO()
 		image_file.download(out=style_image_stream)
 
-		output = model.transfer_style(content_image_stream, style_image_stream)
+		# output = model.transfer_style(content_image_stream, style_image_stream)
+		output = transfer(content_image_stream)
 
 		# теперь отправим назад фото
 		output_stream = BytesIO()
@@ -45,20 +47,111 @@ def send_prediction_on_photo(bot, update):
 		first_image_file[chat_id] = image_file
 
 
+def cancel(context, update):
+	user = update.message.from_user
+	logger.info("User %s canceled the conversation.", user.first_name)
+	update.message.reply_text('Bye! I hope we can talk again some day.',
+							  reply_markup=ReplyKeyboardRemove())
+
+	return ConversationHandler.END
+
+
+def start(context, update):
+	# TODO create list of styles automatically
+	reply_keyboard = [['candy', 'mosaic', 'rain_princess', 'udnie']]
+
+
+	update.message.reply_text(
+		'Hi! My name is Artist Bot. I will make an artwork you. '
+		'Send /cancel to stop talking to me.\n\n'
+		'Which style do you prefer?',
+		reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+	return GENDER
+
+
+def gender(context, update):
+	user = update.message.from_user
+	chat_id = update.message.chat_id
+	info[chat_id] = [update.message.text, None]
+	logger.info("Style of %s: %s", user.first_name, update.message.text)
+	update.message.reply_text('Okay. Now you, dumbass, must send me goddamn picture\n'
+							  'Be fast, faggot!',
+							  reply_markup=ReplyKeyboardRemove())
+
+	return PHOTO
+
+
+def photo(bot, update):
+	# user = update.message.from_user
+	# photo_file = update.message.photo[-1].get_file()
+	# photo_file.download('user_photo.jpg')
+	# logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
+	chat_id = update.message.chat_id
+	update.message.reply_text('Got it! Wait and i will send you the result')
+
+	print("Got image from {}".format(chat_id))
+
+	# получаем информацию о картинке
+	image_info = update.message.photo[-1]
+	image_file = bot.get_file(image_info)
+	first_image_file[chat_id] = image_file
+	# if chat_id in first_image_file:
+
+	# первая картинка, которая к нам пришла станет content image
+	content_image_stream = BytesIO()
+	first_image_file[chat_id].download(out=content_image_stream)
+	info[chat_id][1] = content_image_stream
+	del first_image_file[chat_id]
+
+	output = transfer(info[chat_id][1], info[chat_id][0])
+
+	# теперь отправим назад фото
+	output_stream = BytesIO()
+	output.save(output_stream, format='PNG')
+	output_stream.seek(0)
+	update.message.reply_text('Enjoy!')
+	bot.send_photo(chat_id, photo=output_stream)
+	print("Sent Photo to user")
+	# else:
+	# 	first_image_file[chat_id] = image_file
+
+	return ConversationHandler.END
+
+
 if __name__ == '__main__':
-	from telegram.ext import Updater, MessageHandler, Filters
+	from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
+	from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
+						  ConversationHandler, Filters)
 	import logging
 
 	# Включим самый базовый логгинг, чтобы видеть сообщения об ошибках
 	logging.basicConfig(
 		format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
 		level=logging.INFO)
+	logger = logging.getLogger(__name__)
+	GENDER, PHOTO, LOCATION, BIO = range(4)
 	# используем прокси, так как без него у меня ничего не работало.
 	# если есть проблемы с подключением, то попробуйте убрать прокси или сменить на другой
 	# проекси ищется в гугле как "socks4 proxy"
-	updater = Updater(token=token, request_kwargs={ 'proxy_url': 'socks4://193.107.221.176:4145' })
+	updater = Updater(token=token, request_kwargs={ 'proxy_url': 'socks4://136.243.48.56:1080' })
 
 	# В реализации большого бота скорее всего будет удобнее использовать Conversation Handler
 	# вместо назначения handler'ов таким способом
-	updater.dispatcher.add_handler(MessageHandler(Filters.photo, send_prediction_on_photo))
+	# updater.dispatcher.add_handler(MessageHandler(Filters.photo, send_prediction_on_photo))
+
+	conv_handler = ConversationHandler(
+		entry_points=[CommandHandler('start', start)],
+
+		states={
+			# GENDER: [RegexHandler('^(Boy|Girl|Other)$', gender)],
+			GENDER: [MessageHandler(Filters.text, gender)],
+
+			PHOTO: [MessageHandler(Filters.photo, photo)]
+		},
+
+		fallbacks=[CommandHandler('cancel', cancel)]
+	)
+	updater.dispatcher.add_handler(conv_handler)
+
 	updater.start_polling()
